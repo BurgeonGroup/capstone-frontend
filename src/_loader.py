b4a698b868a3852b320460fa6ca574e0bf539513
mmellott <matthew.mellott@yahoo.com>
2014-02-24 21:35:51 -0700

    Return to old load function.

--- src/_loader.py
+++ src/_loader.py
@@ -17,35 +17,20 @@ def makeprg(setup, loop):
     return loop
 
 def load(device, baud, program):
-    """ Load a bitlash program onto the Arduino. """
     serialport = serial.Serial(device, baud, timeout=0)
     c = fdpexpect.fdspawn(serialport.fd)
-    #c.logfile_read = sys.stdout
-    # synch with the command prompt
     c.sendline('')
     waitprompt(c)
-    # do stuff
-    if(program.find('\n')>=0):
-        #program is many lines
-        text = program.split('\n')
-        for line in text:
-            line = line.strip()
-            print(line)
-            if (len(line) > 0) and (line[0] != '#'):
-                c.sendline(line)
-                waitprompt(c)
-                print("waiting for prompt after twinkle")
-    else:
-        #program is one line
-        if (len(program) > 0) and (program[0] != '#'):
-            c.sendline(program)
+    for line in program:
+        line = line.strip()
+        if (len(line) > 0) and (line[0] != '#'):
+            c.sendline(line)
             waitprompt(c)
     c.close()
-    print("Done")
-    return None
 
 if __name__ == "__main__":
-    #prog = ["pixel(0, 0, green); draw_all()\n"]
-    prog = ["while 1 {twinkle();}\n"]
+    prog = ["function myf {while 1 {twinkle();}}",
+            "function startup {run myf}",
+            "boot"]
     load('/dev/ttyACM0', 57600, prog)
 
