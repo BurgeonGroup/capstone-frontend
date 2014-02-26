8bece881fba841367739d04e20551e6a259c7243
mmellott <matthew.mellott@yahoo.com>
2014-02-25 21:49:03 -0700

    Terminate load process from python.

--- src/_loader.py
+++ src/_loader.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python
 """ Functions in here handle loading bitlash programs onto the Arduino. """
 # import _error # might need this if we actually caught exceptions?
-try: import serial
-except Exception: print "NO SERIAL MODULE DETECTED"
-try: import fdpexpect
-except Exception: print "NO FDPEXPECT MODULE DETECTED"
-import time
+import serial, fdpexpect, time
+from multiprocessing import Process
 
 def waitprompt(c):
     """ Wait until we see a prompt. """
     c.expect('\n> ')
     time.sleep(0.1)
 
-def makeprg(setup, loop):
-    """ We can only handle one line prgs with no setup right now... """
-    return loop
-
 def load(device, baud, program):
     serialport = serial.Serial(device, baud, timeout=0)
     c = fdpexpect.fdspawn(serialport.fd)
     c.sendline('')
     waitprompt(c)
-    for line in program:
+    for line in program.splitlines():
         line = line.strip()
         if (len(line) > 0) and (line[0] != '#'):
             c.sendline(line)
             waitprompt(c)
     c.close()
 
+
+class Loader:
+    def __init__(self, device, baud):
+        self.device = device
+        self.baud = baud
+        self.proc = None
+
+    def load(self, program):
+        if self.proc is not None:
+            self.proc.terminate()
+        self.proc = Process(target=load,
+                            args=(self.device, self.baud, program))
+        self.proc.start()
+
+
 if __name__ == "__main__":
-    prog = ["function myf {while 1 {twinkle();}}",
-            "function startup {run myf}",
-            "boot"]
-    load('/dev/ttyACM0', 57600, prog)
+    l = Loader('/dev/ttyACM0', 57600)
+    while True: l.load(raw_input("> "))
 
